2ee2840c1cc7198977649a6ab931c885afc7a989
bendemouth <bendemouth@gmail.com>
2024-08-26 12:04:05 -0500

    Add to webhost (WORKING)
    All files working as expected on webhost. Will update to add security and fix deletePlay() Function

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    AZURE_CLIENT_ID: 'e653a00a-9730-4bbf-aa23-d72716997aea',
+    AZURE_CLIENT_SECRET: 'Bq98Q~eV1Bs4ba1SL~xNQrrBfWVIM0LFYTr8ab.2',
+    AZURE_TENANT_ID: '662561cd-4fb9-4433-ad1d-2706ee4955a9',
+    DB_SERVER: "pell-city-basketball.database.windows.net",
+    DB_DATABASE: "pell-city-boys-basketball",
+    PORT: 25571
+};
